ea39f55f8e5a811e9d4515ddba73e35135328c4c
cylammarco <cylammarco@gmail.com>
2019-07-11 17:52:11 +0100

    Fixed path of the calibration_test.py. Added Jupyter Notebook that can be viewed on GtiHub. calibrator.py can handle the lack of matplotlib and tqdm. PEP8 formatted with yapf. Removed peakutils and use scipy.signal.find_peaks() instead. Removed polyfit_value() function, use numpy.polyval() instead.

--- calibration_test.py
+++ calibration_test.py
@@ -1,34 +1,47 @@
-from calibrator import Calibrator, getPeaks, polyfit_value
-import astropy.units as u
 import numpy as np
 import matplotlib.pyplot as plt
+import astropy.units as u
+from astropy.io import fits
+from scipy.signal import find_peaks
+from calibrator import Calibrator
 
-def load_calibration_lines(input_file = 'calibration_lines.csv', elements = ["Hg", "Ar", "Xe", "CuNeAr", "Kr"], min_wavelength=350, max_wavelength=800):
 
-    with open(input_file,'r') as calfile:
-        data = calfile.readlines()[1:]
-    
-    cal_lines = [line.replace('\n', '').split(',')[:2] for line in data]
-
-    lines = np.array(np.sort([float(line[0]) for line in cal_lines if line[1] in elements]))
-
-    mask = (lines > min_wavelength)*(lines < max_wavelength)
-    
+def load_calibration_lines(input_file='calibration_lines.csv',
+                           elements=["Hg", "Ar", "Xe", "CuNeAr", "Kr"],
+                           min_wavelength=300,
+                           max_wavelength=800):
+    cal_lines = np.loadtxt(input_file, delimiter=',', dtype='U', skiprows=1)
+    wave = cal_lines[:, 0].astype('float')
+    element = cal_lines[:, 1]
+    # Get lines of the requested elements
+    lines = wave[np.isin(element, elements)]
+    # Get only lines within the requested wavelength
+    mask = (lines > min_wavelength) * (lines < max_wavelength)
     return lines[mask]
 
-spectrum = np.loadtxt("C:/Users/Josh/Downloads/A620EBA HgCal.mspec", delimiter=',')[:,1]
-atlas = load_calibration_lines("C:/Users/Josh/Downloads/calibration_lines.csv", elements = ["Hg", "Ar"], min_wavelength=380)
 
-peaks, _ = getPeaks(np.log(spectrum), min_dist=100, thres=0.5)
+atlas = load_calibration_lines(
+    "calibration_lines.csv", elements=["Xe"], min_wavelength=400, max_wavelength=800)
+
+spectrum = np.median(fits.open('v_a_20190516_55_1_0_1.fits')[0].data, axis=0)[200:]
+
+peaks, _ = find_peaks(spectrum, distance=3., threshold=1.)
+plt.plot(spectrum)
+plt.vlines(peaks,
+              spectrum[peaks.astype('int')],
+              spectrum.max(),
+              colors='red',
+              alpha=0.5)
 
 c = Calibrator(peaks, atlas)
-c.set_fit_constraints(min_slope=0.1,
-                      max_slope=0.15,
-                      min_intercept=375,
-                      max_intercept=425,
-                      line_fit_thresh=5)
- 
+c.set_fit_constraints(
+    min_slope=0.01,
+    max_slope=100.0,
+    min_intercept=0,
+    max_intercept=10000,
+    fit_tolerance=0.5,
+    line_fit_thresh=10)
+
 best_p = c.fit()
 print(best_p)
 c.plot_fit(spectrum, best_p)
-
--- calibrator.py
+++ calibrator.py
@@ -1,31 +1,19 @@
 import itertools
 import numpy as np
 import astropy.units as u
-import peakutils
-import matplotlib.pyplot as plt
-from tqdm.autonotebook import tqdm
+try:
+    import matplotlib.pyplot as plt
+    matplotlib_imported = True
+except:
+    print('matplotlib package not available. Plot cannot be generated.')
+    matplotlib_imported = False
+try:
+    from tqdm.autonotebook import tqdm
+    tdqm_imported = True
+except:
+    print('tqdm package not available. Progress bar will not be shown.')
+    tdqm_imported = False
 
-def getPeaks(data, thres=0.1, min_dist=40, obs_range=(400,850)):
-    
-    x = np.arange(len(data))
-    x_approx = np.linspace(obs_range[0], obs_range[1], len(x))
-    y = data
-    
-    idx = peakutils.indexes(y, thres, min_dist)
-
-    peaks_x = []
-    peaks_approx = []
-    for i in idx:
-        try:
-            peaks_x.append(peakutils.interpolate(x, y, ind=[i]))
-            peaks_approx.append(peakutils.interpolate(x_approx, y, ind=[i]))
-        except:
-            pass
-    
-    return np.array(peaks_x).flatten(), np.array(peaks_approx).flatten()
-
-def polyfit_value(x, p):
-    return np.sum([p[n]*np.array(x).astype('float')**n for n in range(len(p))], axis=0)
 
 class Calibrator:
     def __init__(self, peaks, atlas):
@@ -42,8 +30,9 @@ class Calibrator:
 
     def fit(self, top_n=5):
 
-        self.accumulator = self._hough_points(self.pairs[:,0], self.pairs[:,1])
-        
+        self.accumulator = self._hough_points(self.pairs[:, 0],
+                                              self.pairs[:, 1])
+
         h, lines = self._get_top_lines(self.accumulator, bins=100, top_n=top_n)
 
         self.candidates = []
@@ -53,8 +42,15 @@ class Calibrator:
             self.candidates.append((inliers_x, inliers_y))
 
         return self._get_best_model(self.candidates)
-        
-    def set_fit_constraints(self, min_slope=0.1, max_slope=2, min_intercept=200, max_intercept=500, line_fit_thresh=5, fit_tolerance=0.5, inlier_tolerance=1):
+
+    def set_fit_constraints(self,
+                            min_slope=0.1,
+                            max_slope=2,
+                            min_intercept=200,
+                            max_intercept=500,
+                            line_fit_thresh=5,
+                            fit_tolerance=0.5,
+                            inlier_tolerance=1):
         self.min_slope = min_slope
         self.max_slope = max_slope
         self.min_intercept = min_intercept
@@ -62,10 +58,10 @@ class Calibrator:
         self.line_fit_thresh = line_fit_thresh
         self.fit_tolerance = fit_tolerance
         self.inlier_tolerance = inlier_tolerance
-    
+
     def _generate_pairs(self):
-        self.pairs = np.array([pair for pair in itertools.product(self.peaks,
-                                                         self.atlas)])
+        self.pairs = np.array(
+            [pair for pair in itertools.product(self.peaks, self.atlas)])
 
     def _hough_points(self, x, y, num_slopes=3600):
         """
@@ -80,13 +76,13 @@ class Calibrator:
 
         for m in np.linspace(self.min_slope, self.max_slope, num_slopes):
             # y = mx+c -> (y-mx) = c
-            intercepts = y - m*x
+            intercepts = y - m * x
 
             for c in intercepts:
                 # Initial wavelength unlikely to be below 150 nm
                 if c < self.min_intercept:
                     continue
-                    
+
                 if c > self.max_intercept:
                     continue
 
@@ -95,28 +91,36 @@ class Calibrator:
         return np.array(accumulator)
 
     def _get_top_lines(self, accumulator, bins=30, top_n=5):
-        h, xedges, yedges = np.histogram2d(accumulator[:,0], accumulator[:,1], bins=bins)
+        h, xedges, yedges = np.histogram2d(
+            accumulator[:, 0], accumulator[:, 1], bins=bins)
 
-        xbin_width = (xedges[1]-xedges[0])/2
-        ybin_width = (yedges[1]-yedges[0])/2
+        xbin_width = (xedges[1] - xedges[0]) / 2
+        ybin_width = (yedges[1] - yedges[0]) / 2
 
-        top_bins = np.dstack(np.unravel_index(np.argsort(h.ravel())[::-1][:top_n], h.shape))[0]
-        
+        top_bins = np.dstack(
+            np.unravel_index(np.argsort(h.ravel())[::-1][:top_n], h.shape))[0]
+
         lines = []
         for b in top_bins:
-            lines.append((xedges[b[0]]+xbin_width, yedges[b[1]]+ybin_width))
-        
+            lines.append((xedges[b[0]] + xbin_width,
+                          yedges[b[1]] + ybin_width))
+
         return h, lines
 
     def _get_candidate_points(self, m, c, thresh=5):
-    
-        predicted = (m*self.pairs[:,0]+c)
-        actual = self.pairs[:,1]
-        err = np.abs(predicted - actual)
-        
-        return self.pairs[:,0][err < thresh], self.pairs[:,1][err < thresh]
 
-    def _solve_candidate_ransac(self, x, y, polydeg=3, thresh=1, brute_force=False):
+        predicted = (m * self.pairs[:, 0] + c)
+        actual = self.pairs[:, 1]
+        err = np.abs(predicted - actual)
+
+        return self.pairs[:, 0][err < thresh], self.pairs[:, 1][err < thresh]
+
+    def _solve_candidate_ransac(self,
+                                x,
+                                y,
+                                polydeg=3,
+                                thresh=1,
+                                brute_force=False):
 
         valid_solution = False
         best_inliers = [False]
@@ -125,13 +129,13 @@ class Calibrator:
         best_err = 1e50
         max_tries = 3e4
         sample_size = polydeg + 1
-        
+
         x = np.array(x)
         y = np.array(y)
-        
+
         if len(np.unique(x)) <= polydeg:
             return (best_p, best_err, sum(best_inliers), False)
-        
+
         idx = range(len(x))
 
         if brute_force:
@@ -140,7 +144,11 @@ class Calibrator:
             sampler = range(int(max_tries))
 
         # Brute force check all combinations. N choose 4 is pretty fast.
-        for sample in tqdm(sampler):
+        if tdqm_imported:
+            sampler_list = tqdm(sampler)
+        else:
+            sampler_list = sampler
+        for sample in sampler_list:
 
             if brute_force:
                 x_hat = x[[sample]]
@@ -149,49 +157,49 @@ class Calibrator:
                 idxes = np.random.choice(idx, sample_size, replace=False)
                 x_hat = x[idxes]
                 y_hat = y[idxes]
-            
+
             # Ignore samples with duplicate x/y coordinates
-            if(len(x_hat) > len(np.unique(x_hat))):
+            if (len(x_hat) > len(np.unique(x_hat))):
                 continue
-            
-            if(len(y_hat) > len(np.unique(y_hat))):
+
+            if (len(y_hat) > len(np.unique(y_hat))):
                 continue
-                
+
             # Try to fit the data
             fit_coeffs = np.polyfit(x_hat, y_hat, polydeg)
-            
+
             # Discard out-of-bounds fits
-            if(fit_coeffs[-1] < self.min_intercept):
+            if (fit_coeffs[-1] < self.min_intercept):
                 continue
-                
-            if(fit_coeffs[-1] > self.max_intercept):
+
+            if (fit_coeffs[-1] > self.max_intercept):
                 continue
-            
-            if(fit_coeffs[-2] < self.min_slope):
+
+            if (fit_coeffs[-2] < self.min_slope):
                 continue
-            
-            if(fit_coeffs[-2] > self.max_slope):
+
+            if (fit_coeffs[-2] > self.max_slope):
                 continue
-            
+
             # M-SAC Estimator (Torr and Zisserman, 1996)
-            err = np.abs(polyfit_value(x, fit_coeffs[::-1]) - y)
+            err = np.abs(np.polyval(fit_coeffs, x) - y)
             err[err > thresh] = thresh
             cost = sum(err)
             inliers = err < thresh
-            
+
             # Want the most inliers with the lowest error
             if cost <= best_cost:
                 best_inliers = inliers
                 best_cost = cost
-
+                
                 best_p = np.polyfit(x[best_inliers], y[best_inliers], polydeg)
-                err = np.abs(polyfit_value(x[best_inliers], fit_coeffs[::-1]) - y[best_inliers])
+                err = np.abs(np.polyval(best_p, x[best_inliers]) - y[best_inliers])
                 best_err = err.mean()
 
                 # Perfect fit, break early
                 if sum(best_inliers) == len(x):
                     break
-        
+            
         # Overfit check
         if sum(best_inliers) == polydeg + 1:
             valid_solution = False
@@ -201,17 +209,20 @@ class Calibrator:
         print(best_p)
 
         return (best_p, best_err, sum(best_inliers), valid_solution)
-    
+
     def _get_best_model(self, candidates):
 
         best_inliers = 0
         best_p = None
         best_err = 1e10
-        
-        for candidate in tqdm(candidates):
+        if tdqm_imported:
+            candidate_list = tqdm(candidates)
+        else:
+            candidate_list = candidates
+        for candidate in candidate_list:
             x, y = candidate
-            p, err, n_inliers, valid = self._solve_candidate_ransac(x, y,
-                                                    thresh=self.inlier_threshold)
+            p, err, n_inliers, valid = self._solve_candidate_ransac(
+                x, y, thresh=self.inlier_threshold)
 
             if valid == False:
                 print("Invalid fit")
@@ -241,31 +252,42 @@ class Calibrator:
         y_match = []
 
         for p in self.peaks:
-            x = polyfit_value(p, fit[::-1])
+            x = np.polyval(p, fit)
             diff = np.abs(self.atlas - x)
             idx = np.argmin(diff)
 
             if diff[idx] < tolerance:
                 x_match.append(p)
                 y_match.append(self.atlas[idx])
-        
+
         return np.polyfit(x_match, y_match, 4)
 
-
     def plot_fit(self, spectrum, fit, tolerance=0.5):
-        plt.figure(figsize=(16,8))
-        plt.plot(polyfit_value(np.arange(len(spectrum)).astype('float'), fit[::-1]), spectrum)
-        plt.vlines(polyfit_value(self.peaks, fit[::-1]), spectrum[self.peaks.astype('int')], spectrum.max(), colors='red', alpha=0.5)
+        if matplotlib_imported:
+            plt.figure(figsize=(16, 8))
+            plt.plot(
+                np.polyval(fit, np.arange(len(spectrum)).astype('float')), spectrum)
+            plt.vlines(
+                np.polyval(fit, self.peaks),
+                spectrum[self.peaks.astype('int')],
+                spectrum.max(),
+                colors='red',
+                alpha=0.5)
 
-        for p in self.peaks:
-            x = polyfit_value(p, fit[::-1])
-            diff = np.abs(self.atlas - x)
-            idx = np.argmin(diff)
+            for p in self.peaks:
+                x = np.polyval(fit, p)
+                diff = np.abs(self.atlas - x)
+                idx = np.argmin(diff)
 
-            print("Peak at: {} nm".format(x))
+                print("Peak at: {} nm".format(x))
 
-            if diff[idx] < tolerance:
-                print("- matched to {} nm".format(self.atlas[idx]))
-                plt.text(x-3, 0.8*max(spectrum), s="{:1.2f}".format(self.atlas[idx]),rotation=90, bbox=dict(facecolor='white', alpha=1))
-        
-        plt.show()
+                if diff[idx] < tolerance:
+                    print("- matched to {} nm".format(self.atlas[idx]))
+                    plt.text(
+                        x - 3,
+                        0.8 * max(spectrum),
+                        s="{:1.2f}".format(self.atlas[idx]),
+                        rotation=90,
+                        bbox=dict(facecolor='white', alpha=1))
+
+            plt.show()
