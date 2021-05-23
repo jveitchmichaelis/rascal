ab99e70d8f8a747ccbd15e1a08f7a961d9c09521
cylammarco <cylammarco@gmail.com>
2021-05-23 12:14:02 +0300

    gemini stats script

--- journal_article/lt_sprat_plot.py
+++ journal_article/lt_sprat_plot.py
@@ -11,10 +11,12 @@ else:
     base_dir = os.getcwd()
 
 colors = list(plt.cm.tab10(np.arange(10))) + ["black", "firebrick"]
-mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=colors) 
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=colors)
+
 
 def flip(items, ncol):
     return itertools.chain(*[items[i::ncol] for i in range(ncol)])
+
 
 max_tries = [25, 50, 75, 100, 150, 200, 250, 500, 1000, 2000, 5000]
 
@@ -23,19 +25,23 @@ N = 1000
 
 best_p_mt = np.load(os.path.join(base_dir, 'best_p_mt.npy'))
 rms_mt = np.load(os.path.join(base_dir, 'rms_mt.npy'))
-residual_mt = np.load(os.path.join(base_dir, 'residual_mt.npy'), allow_pickle=True)
-peak_utilisation_mt = np.load(os.path.join(base_dir, 'peak_utilisation_mt.npy'))
+residual_mt = np.load(os.path.join(base_dir, 'residual_mt.npy'),
+                      allow_pickle=True)
+peak_utilisation_mt = np.load(os.path.join(base_dir,
+                                           'peak_utilisation_mt.npy'))
 '''
 best_p_manual_mt = np.load(os.path.join(base_dir, 'best_p_manual_mt.npy'))
 rms_manual_mt = np.load(os.path.join(base_dir, 'rms_manual_mt.npy'))
-residual_manual_mt = np.load(os.path.join(base_dir, 'residual_manual_mt.npy'), allow_pickle=True)
+residual_manual_mt = np.load(
+    os.path.join(base_dir, 'residual_manual_mt.npy'), allow_pickle=True)
 peak_utilisation_manual_mt = np.load(
     os.path.join(base_dir, 'peak_utilisation_manual_mt.npy'))
 '''
 
 best_p_manual_mt = np.load(os.path.join(base_dir, 'best_p_mt.npy'))
 rms_manual_mt = np.load(os.path.join(base_dir, 'rms_mt.npy'))
-residual_manual_mt = np.load(os.path.join(base_dir, 'residual_mt.npy'), allow_pickle=True)
+residual_manual_mt = np.load(os.path.join(base_dir, 'residual_mt.npy'),
+                             allow_pickle=True)
 peak_utilisation_manual_mt = np.load(
     os.path.join(base_dir, 'peak_utilisation_mt.npy'))
 
@@ -99,7 +105,6 @@ for i, mt in enumerate(max_tries):
                    range=p4_range_manual,
                    histtype='step')
 
-
 ax2[0, 0].grid()
 ax2[0, 1].grid()
 ax2[0, 2].grid()
@@ -123,7 +128,11 @@ ax2[1, 0].set_ylabel('Manual lines')
 
 handles, labels = ax2[0, 0].get_legend_handles_labels()
 
-fig2.legend(flip(handles, 6), flip(labels, 6), loc='lower center', mode='expand', ncol=6)
+fig2.legend(flip(handles, 6),
+            flip(labels, 6),
+            loc='lower center',
+            mode='expand',
+            ncol=6)
 fig2.tight_layout(w_pad=0.01)
 fig2.subplots_adjust(bottom=0.1)
 
@@ -137,9 +146,9 @@ fig3.set_figwidth(6)
 ax3[0].violinplot(rms_mt.T, showmedians=True)
 ax3[1].violinplot(rms_manual_mt.T, showmedians=True)
 
-ax3[0].set_xticks(range(1, len(max_tries)+1))
+ax3[0].set_xticks(range(1, len(max_tries) + 1))
 ax3[0].set_xticklabels(max_tries)
-ax3[1].set_xticks(range(1, len(max_tries)+1))
+ax3[1].set_xticks(range(1, len(max_tries) + 1))
 ax3[1].set_xticklabels(max_tries)
 
 ax3[0].grid()
@@ -157,13 +166,12 @@ fig4, ax4 = plt.subplots(2, 1, sharex=True, sharey=True)
 fig4.set_figheight(6)
 fig4.set_figwidth(6)
 
-ax4[0].violinplot(peak_utilisation_mt.T * 100.,
-                        showmedians=True)
+ax4[0].violinplot(peak_utilisation_mt.T * 100., showmedians=True)
 ax4[1].violinplot(peak_utilisation_manual_mt.T * 100., showmedians=True)
 
-ax4[0].set_xticks(range(1, len(max_tries)+1))
+ax4[0].set_xticks(range(1, len(max_tries) + 1))
 ax4[0].set_xticklabels(max_tries)
-ax4[1].set_xticks(range(1, len(max_tries)+1))
+ax4[1].set_xticks(range(1, len(max_tries) + 1))
 ax4[1].set_xticklabels(max_tries)
 
 ax4[0].grid()
@@ -189,7 +197,6 @@ for i, mt in enumerate(max_tries):
         wave_manual[i][j] = np.polynomial.polynomial.polyval(
             pix, best_p_manual)
 
-
 pix0_range = np.nanpercentile(wave[-1][:, 0], [1., 99.])
 pix1_range = np.nanpercentile(wave[-1][:, 1], [1., 99.])
 pix2_range = np.nanpercentile(wave[-1][:, 2], [1., 99.])
@@ -200,7 +207,6 @@ pix1_range_manual = np.nanpercentile(wave_manual[-1][:, 1], [1., 99.])
 pix2_range_manual = np.nanpercentile(wave_manual[-1][:, 2], [1., 99.])
 pix3_range_manual = np.nanpercentile(wave_manual[-1][:, 3], [1., 99.])
 pix4_range_manual = np.nanpercentile(wave_manual[-1][:, 4], [1., 99.])
-
 
 fig5, ax5 = plt.subplots(2, 5, sharey=True)
 fig5.set_figheight(10)
@@ -256,13 +262,14 @@ ax5[1, 0].set_ylabel('Frequency')
 
 handles, labels = ax5[0, 0].get_legend_handles_labels()
 
-fig5.legend(flip(handles, 6), flip(labels, 6), loc='lower center', mode='expand', ncol=6)
+fig5.legend(flip(handles, 6),
+            flip(labels, 6),
+            loc='lower center',
+            mode='expand',
+            ncol=6)
 fig5.tight_layout(w_pad=0.01)
 fig5.subplots_adjust(bottom=0.1)
 fig5.savefig('figure_5_wavelengths.png')
-
-
-
 
 # Figure 6 - 2D heatmap of the solution
 fig6, ax6 = plt.subplots(2, 1, sharex=True, sharey=True)
@@ -303,25 +310,26 @@ dw_manual_yedges = np.histogram(delta_wave_manual,
                                 range=(dw_manual_min, dw_manual_max))[1]
 
 ax6[0].imshow(np.array(delta_wave_heatmap).T, origin='lower', aspect='auto')
-ax6[0].set_yticks(np.linspace(0, 40, len(dw_yedges[::10]))-0.5)
+ax6[0].set_yticks(np.linspace(0, 40, len(dw_yedges[::10])) - 0.5)
 ax6[0].set_yticklabels(dw_yedges[::10].astype('int'))
 ax6[0].set_ylabel(r'$\Delta\lambda\ /\ \AA$')
 
 ax6a = ax6[0].twiny()
-ax6a.set_xticks(pix[::200]-1)
+ax6a.set_xticks(pix[::200] - 1)
 ax6a.set_xticklabels(np.nanmedian(wave, axis=0)[::200].astype('int'))
 ax6a.set_xlabel(r'Wavelength / $\AA$')
 
-ax6[1].imshow(np.array(delta_wave_manual_heatmap).T, origin='lower', aspect='auto')
-ax6[1].set_yticks(np.linspace(0, 40, len(dw_manual_yedges[::10]))-0.5)
+ax6[1].imshow(np.array(delta_wave_manual_heatmap).T,
+              origin='lower',
+              aspect='auto')
+ax6[1].set_yticks(np.linspace(0, 40, len(dw_manual_yedges[::10])) - 0.5)
 ax6[1].set_yticklabels(dw_manual_yedges[::10].astype('int'))
 
 ax6[1].set_ylabel(r'$\Delta\lambda\ /\ \AA$')
-ax6[1].set_xticks(pix[::200]-1)
-ax6[1].set_xticklabels((pix[::200]-1).astype('int'))
+ax6[1].set_xticks(pix[::200] - 1)
+ax6[1].set_xticklabels((pix[::200] - 1).astype('int'))
 ax6[1].set_xlabel('Pixel')
 
 fig6.tight_layout()
 fig6.subplots_adjust(hspace=0.05)
 fig6.savefig('figure_6_heatmap.png')
-
