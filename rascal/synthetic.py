f09d3737a99c8bb0f916900666135eec49ae1316
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2021-07-05 21:03:16 +1200

    add inverse cases for 0 and 1 degree poly

--- rascal/synthetic.py
+++ rascal/synthetic.py
@@ -24,8 +24,8 @@ class SyntheticSpectrum:
         self.set_wavelength_limit(min_wavelength, max_wavelength)
 
         if coefficients is not None:
-
-            self.set_model(coefficients)
+            self.coefficients = coefficients
+            self.set_model(self.coefficients)
 
         else:
 
@@ -106,11 +106,18 @@ class SyntheticSpectrum:
         wavelengths = np.array(wavelengths)
         wavelengths = wavelengths[wavelengths > self.min_wavelength]
         wavelengths = wavelengths[wavelengths < self.max_wavelength]
+        
+        # Constant function y = c
+        if self.degree == 0:
+            pixels = self.coefficients[0]*np.ones(len(wavelengths))
+        # Linear function y = mx + c
+        elif self.degree == 1:
+            #x = (y - c) / m
+            pixels = (wavelengths - self.coefficients[0]) / self.coefficients[1]
+        else:
+            pixels = pynverse.inversefunc(self.model, wavelengths)
 
-        pixels = pynverse.inversefunc(self.model, wavelengths)
-
-        return pixels
-
+        return pixels, wavelengths
 
 '''
 class RandomSyntheticSpectrum(SyntheticSpectrum):
