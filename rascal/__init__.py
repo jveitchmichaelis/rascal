8c859d18b7c769559ca9ff47977c47a121223b28
cylammarco <cylammarco@gmail.com>
2022-02-03 15:43:06 +0200

    fixed pathing issue

--- rascal/__init__.py
+++ rascal/__init__.py
@@ -10,7 +10,6 @@ META_PATH = os.path.join("..", "pyproject.toml")
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 
-
 def read(*parts):
     with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
         return f.read()
