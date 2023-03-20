49f38c1306a5ae056a1e0c464c0076f463eaa61c
cylammarco <cylammarco@gmail.com>
2023-03-20 20:19:12 +0200

    auto linelist not working

--- examples/example_lt_sprat_auto_atlas.py
+++ examples/example_lt_sprat_auto_atlas.py
@@ -17,67 +17,80 @@ fits_file = fits.open(
 
 spectrum2D = fits_file.data
 
-plt.ion()
 plt.figure(1, figsize=(10, 4))
 plt.imshow(np.log10(spectrum2D), aspect="auto", origin="lower")
 plt.xlabel("Spectral Direction / Pix")
 plt.ylabel("Spatial Direction / Pix")
 plt.tight_layout()
 plt.savefig(
-    os.path.join(base_dir, "output", "lt-sprat-auto-atlas-arc-image.png")
+    os.path.join(
+        base_dir, "output_lt_sprat", "lt-sprat-manual-atlas-arc-image.png"
+    )
 )
 
+# Collapse into 1D spectrum between row 110 and 120
+spectrum = np.median(spectrum2D[110:120], axis=0)
 
 temperature = fits_file.header["REFTEMP"]
 pressure = fits_file.header["REFPRES"] * 100.0
 relative_humidity = fits_file.header["REFHUMID"]
 
-# Collapse into 1D spectrum between row 110 and 120
-spectrum = np.median(spectrum2D[110:120], axis=0)
-
 # Identify the peaks
-peaks, _ = find_peaks(spectrum, height=300, prominence=200, distance=5)
-peaks = util.refine_peaks(spectrum, peaks, window_width=5)
+peaks, _ = find_peaks(
+    spectrum, height=200, prominence=100, distance=5, threshold=None
+)
+peaks = util.refine_peaks(spectrum, peaks, window_width=3)
 
-# Initialise the calibrator
-c = Calibrator(peaks, spectrum=spectrum)
-c.plot_arc(
-    display=False,
-    save_fig="png",
-    filename=os.path.join(
-        base_dir, "output", "lt-sprat-auto-atlas-arc-spectrum"
-    ),
-)
-
-c.set_hough_properties(
-    num_slopes=2000,
-    range_tolerance=200.0,
-    xbins=100,
-    ybins=100,
-    min_wavelength=3600.0,
-    max_wavelength=8000.0,
-)
-c.set_ransac_properties(
-    sample_size=5, top_n_candidate=5, filter_close=True, minimum_matches=13
-)
+config = {
+    "data": {
+        "contiguous_range": None,
+        "detector_min_wave": 3500.0,
+        "detector_max_wave": 8200.0,
+        "detector_edge_tolerance": 200.0,
+        "num_pix": 1024,
+    },
+    "hough": {
+        "num_slopes": 2000,
+        "range_tolerance": 200.0,
+        "xbins": 200,
+        "ybins": 200,
+    },
+    "ransac": {
+        "max_tries": 5000,
+        "inlier_tolerance": 8.0,
+        "sample_size": 8,
+        "top_n_candidate": 5,
+        "filter_close": False,
+    },
+}
 
 atlas = Atlas(
     elements=["Xe"],
     min_intensity=50.0,
-    min_distance=10,
-    min_atlas_wavelength=3600.0,
-    max_atlas_wavelength=8000.0,
+    min_wavelength=3800.0,
+    max_wavelength=8200.0,
     range_tolerance=200.0,
     pressure=pressure,
     temperature=temperature,
     relative_humidity=relative_humidity,
+    brightest_n_lines=40,
 )
-c.set_atlas(atlas, candidate_tolerance=5.0)
 
-c.do_hough_transform()
+# Initialise the calibrator
+c = Calibrator(
+    peaks, atlas_lines=atlas.atlas_lines, config=config, spectrum=spectrum
+)
 
 # Run the wavelength calibration
-res = c.fit(max_tries=2000)
+res = c.fit()
+
+c.plot_arc(
+    display=False,
+    save_fig="png",
+    filename=os.path.join(
+        base_dir, "output_lt_sprat", "lt-sprat-manual-atlas-arc-spectrum"
+    ),
+)
 
 # Plot the solution
 c.plot_fit(
@@ -88,7 +101,9 @@ c.plot_fit(
     log_spectrum=False,
     save_fig="png",
     filename=os.path.join(
-        base_dir, "output", "lt-sprat-auto-atlas-wavelength-calibration"
+        base_dir,
+        "output_lt_sprat",
+        "lt-sprat-manual-atlas-wavelength-calibration",
     ),
 )
 
@@ -96,7 +111,7 @@ c.plot_fit(
 c.plot_search_space(
     save_fig="png",
     filename=os.path.join(
-        base_dir, "output", "lt-sprat-auto-atlas-search-space"
+        base_dir, "output_lt_sprat", "lt-sprat-manual-atlas-search-space"
     ),
 )
 
