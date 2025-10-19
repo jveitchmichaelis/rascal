ac7bec992786618e1f01b7b7d6a07788e6f86d8c
cylammarco <cylammarco@gmail.com>
2025-10-19 15:28:02 +0100

    more codacy fix

--- test/test_lt_sprat_manual_atlas.py
+++ test/test_lt_sprat_manual_atlas.py
@@ -120,7 +120,7 @@ def test_sprat_manual_atlas():
     c.do_hough_transform(brute_force=True)
 
     # Run the wavelength calibration
-    best_p, _, _, rms, residual, peak_utilisation, atlas_utilisation = c.fit(max_tries=250)
+    best_p, _, _, _, residual, peak_utilisation, atlas_utilisation = c.fit(max_tries=250)
 
     # Plot the solution
     c.plot_fit(
@@ -137,9 +137,9 @@ def test_sprat_manual_atlas():
 
     (
         fit_coeff_new,
-        peak_matched,
-        atlas_matched,
-        rms,
+        _,
+        _,
+        _,
         residual,
         peak_utilisation,
         atlas_utilisation,
