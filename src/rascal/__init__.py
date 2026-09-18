eba0646ca8bdc7f353b7e3ff7dd0a6bf65faf01f
cylammarco <cylammarco@gmail.com>
2026-09-18 11:34:39 +0100

    updated config

--- src/rascal/__init__.py
+++ src/rascal/__init__.py
@@ -8,14 +8,20 @@ try:
 except PackageNotFoundError:
     __version__ = "unknown"
 
+from . import atlas
 from . import calibrator
+from . import houghtransform
 from . import models
+from . import plotting
 from . import synthetic
 from . import util
 
 __all__ = [
+    "atlas",
     "calibrator",
+    "houghtransform",
     "models",
+    "plotting",
     "synthetic",
     "util",
 ]
